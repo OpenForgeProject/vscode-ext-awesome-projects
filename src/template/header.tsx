80a6dde3d66e324a81d1e128c106f0f2d9d1a1a1
Mathias Elle <m.elle@e3n.de>
2025-01-18 00:30:46 +0100

    Refactor header button SVG: simplify icon design for improved clarity

--- src/template/header.tsx
+++ src/template/header.tsx
@@ -20,10 +20,9 @@ const Header: React.FC = () => {
 
       <div className="linkbar">
         <a href="#" className="button">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <path d="M20 6v4a1 1 0 0 1 -1 1h-14a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h14a1 1 0 0 1 1 1z" />
-            <path d="M12 15l0 4" />
-            <path d="M14 17l-4 0" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+            <path stroke="none" d="M0 0h24v24H0z"/>
+            <path d="M12 5v14M5 12h14"/>
           </svg>
           Add
         </a>
