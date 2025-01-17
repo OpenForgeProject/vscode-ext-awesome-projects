8afb206a6680cc707c9179325dd593e773b14454
Mathias Elle <m.elle@e3n.de>
2025-01-18 00:31:44 +0100

    Remove unnecessary whitespace in header component for cleaner code

--- src/template/header.tsx
+++ src/template/header.tsx
@@ -17,7 +17,6 @@ const Header: React.FC = () => {
           </small>
         </div>
       </div>
-
       <div className="linkbar">
         <a href="#" className="button">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
